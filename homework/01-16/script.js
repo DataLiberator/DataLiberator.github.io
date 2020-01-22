3caa1a1692c1ce258e1c2d7920b2ee374e56d5b0
DataLiberator <43225206+DataLiberator@users.noreply.github.com>
2020-01-22 10:00:35 -0500

    Progress at 10am (Wed 01-22)

--- homework/01-16/script.js
+++ homework/01-16/script.js
@@ -1,4 +1,34 @@
-var width = document.querySelector("#chart").clientWidth;
-var height = document.querySelector("#chart").clientHeight;
-var margin = {top: 50, left: 150, right: 50, bottom: 150};
+// Set width, height, SVG variables
+
+var w = document.querySelector("#chart").clientWidth;
+var h = document.querySelector("#chart").clientHeight;
+
+//Create SVG element
+var svg = d3.select("#chart")
+.append("svg")
+.attr("width", w)
+.attr("height", h);
+
+// svgDoc.append("g").selectAll("circle")
+//                 .data(eval("dataArray"+dataIndex))
+//                 .enter()
+//                 .append("circle")
+//                 .attr("cx",function(d,i){
+//                     var spacing = lineLength/(eval("dataArray"+dataIndex).length);
+//                     return xBuffer+(i*spacing)
+//                 })
+//                 .attr("cy",yBuffer)
+//                 .attr("r",function(d,i){return d});
+
+
+
+//Append the static baseline circle
+svg.append("g")
+    .append("circle")
+    .attr("cx", w/2)
+    .attr("cy", h/2)
+    .attr("r", w/3)
+    .style("fill", "none")
+    .style("stroke-width", "3px")
+    .style("stroke", "black");
 
