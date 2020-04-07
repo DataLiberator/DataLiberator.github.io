8e7de4abac23e7bfdce3faca26da924138c10c8d
libbyt21 <home.libby.thomas@gmail.com>
2020-04-07 13:47:31 -0600

    prep

--- FinalProject/script.js
+++ FinalProject/script.js
@@ -1,12 +1,46 @@
+ 
+ d3.csv("./data/Artworks80YrData.csv", function(data){
 
-d3.csv("./data/Artworks80YrData.csv", function(artworksData){
-
+  findYear = function(d){
+    year = parse
+  };
     
-console.log(artworksData);
+    
+    console.log(data.);
+
+    // console.log(data.DateAcquired); //OK, at least this did something. Lists all dates and how many things were acquired on those dates
+
+// ------ CHART #1: Line chart of acquisitions per month between the two years ----------
+
+    // Count # of objects in 1937, 2017
+
+    // Sum acquisition amounts by month for each year.
+
+    //line chart
+
+// ------ CHART #2: Nationality Dots ----------
+//Dots of one color represent # of distinct nationalities per year, size based on # acquisitions. Hover for year and number
+
+    // Find n of distinct nationalities for both years.
+    // Find count of acquisitions for each nationality for both years
+    // Enter/exit/update of dots based on nationality, r scale by # acquisitions.
+    // Tooltip to hover for nationality and number.
+    // (Two separate charts, next to each other.)
+
+// ------- CHART #3: Grouped bar chart by year, show % of M/F/U.
+    // Find % M/F/U by year
+    // Set up scales
+    // Enter rect svgs for each year.
+
+
+
+//PARSE?
+// Split the dates into 1937 and 2017 separately, year and month
+// 
+
+
 
 });
 
 
 
-
-  
