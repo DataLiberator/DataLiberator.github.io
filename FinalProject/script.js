b3ef6863c145a00b1f1c7fc792f3eb6f2551f9f7
libbyt21 <home.libby.thomas@gmail.com>
2020-04-09 15:31:22 -0600

    updates

--- FinalProject/script.js
+++ FinalProject/script.js
@@ -1,6 +1,10 @@
  
  d3.csv("./data/Artworks80YrData.csv", function(data){
 
+    var width = document.querySelector("#chart").clientWidth;
+    var height = document.querySelector("#chart").clientHeight;
+    var margin = {top: 50, left: 150, right: 50, bottom: 150};
+
     console.log(data);
 
     var lineData = d3.nest()
