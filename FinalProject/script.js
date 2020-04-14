16cbe59954bb82e869c8d31db969b26ffafe7239
libbyt21 <home.libby.thomas@gmail.com>
2020-04-14 16:11:24 -0600

    added dots!

--- FinalProject/script.js
+++ FinalProject/script.js
@@ -15,15 +15,22 @@
     .attr("height", height)
     .style("background-color", "#F5F5F5");
 
+    var div = d3.select("body").append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0);
+
     // Color picker
     function colorPicker(d){
         console.log(d[0].year);
 
         if (d[0].year == "1937") { return "#000000"}
         else {return "#cc0000"};
-
     };
 
+    //Format months from numbers to words
+    function month(d){
+        return d3.timeFormat("%b")(new Date((d+1) + "/01/2017"))
+    };
 
     var lineData = d3.nest()
         .key(function(d){
@@ -36,6 +43,7 @@
         d.totalPieces = d.values.length;
         d.date = new Date(d.key)
         d.year = d.date.getFullYear();
+        d.month = d.date.getMonth();
     });
 
     console.log(lineData);
@@ -73,35 +81,7 @@ var yScale = d3.scaleLinear()
 //http://www.dave-landry.com/classes/artg5430-spring2020/lectures/03_12_code.html
 // Focus on how it calculates the x + y (keep it simple!)
 
-// Tooltip info
-// function tooltip(d){
-//     var xPosition = parseFloat(d3.select(this).attr("x")) + xScale.bandwidth() / 2;
-//     var yPosition = parseFloat(d3.select(this).attr("y")) / 2 + height / 2;
-//     d3.select("#tooltip")
-//         .style("left", xPosition + "px")
-//         .style("top", yPosition + "px")
-//         .select("#value")
-//         .text(d);
-//     d3.select("#tooltip").classed("hidden", false);
-// };
-
-// function tooltipOut(d){
-//     d3.select("#tooltip").classed("hidden", true);
-//    };
-
-// Append Paths and Lines
-// lineData.forEach(function(d){
-//      svg.append("path")
-//     .data([data1937])
-//     .attr("class", "line")
-//     .attr("d", line)
-//     .style("fill", "none")
-//     .style("stroke-width", "3")
-//     .style("stroke", "black");
-//     // .on("mouseover", tooltip())
-//     // .on("mouseout", tooltipOut());
-// });
-
+//Add lines to chart
 svg.selectAll(".line")
         .data([data1937, data2017])
         .enter()
@@ -112,9 +92,28 @@ svg.selectAll(".line")
         .style("stroke-width", "3")
         .style("stroke", colorPicker);
 
-function month(d){
-    return d3.timeFormat("%b")(new Date((d+1) + "/01/2017"))
-};
+//Add dots and tooltip to chart
+//Inspired by: https://bl.ocks.org/wnghdcjfe/6377d75c963e8f841609a7bf6d3d0c74
+var dot = svg.selectAll("dot")
+                .data(lineData)
+                .enter().append("circle")
+                .attr("r", 5);
+
+dot.attr("cx", function (d) {
+    return xScale(d.month);
+})
+.attr("cy", function (d) {
+    return yScale(d.totalPieces);
+})
+.on("mouseover", function (d) {
+    div.transition()
+        .duration(100)
+        .style("opacity", .9);
+    div.html("<p> Acquisitons in " + month + ": </p> <p>" + d.totalPieces + "</p>")
+        .style("left", (d3.event.pageX) + "px")
+        .style("top", (d3.event.pageY - 28) + "px");
+});
+
 
 // axes
 var xAxis = svg.append("g")
@@ -139,6 +138,54 @@ var xAxisLabel = svg.append("text")
         .attr("x",-height/1.5)
         .attr("y", (margin.left-50))
         .text("Number of Acquisitions");
- // enter/exit/update
+
+var chartTitle = svg.append("text")
+        .attr("class","axisLabel")
+        .attr("x", width/3)
+        // .attr("text-align", "center")
+        .attr("y", height - (height-margin.top))
+        .text("MoMA Acquisitions per Month");
+
+//handmade legend hijinx (Code source: https://www.d3-graph-gallery.com/graph/custom_legend.html)
+var legend = d3.select("#legend");
+var square = 15;
+var spacing = 30;
+var whiteHeight = 10;
+var redHeight = 45;
+var offset = 13;
+
+legend.append("rect")
+  .attr("width", square)
+  .attr("height",square)
+  .attr("x", 0)
+  .attr("y", whiteHeight)
+  .style("fill", "black");
+
+legend.append("rect")
+  .attr("width", square)
+  .attr("height",square)
+  .attr("x", 0)
+  .attr("y", redHeight)
+  .style("fill", "#CC0000");
+
+legend.append("text")
+  .attr("id", "legendText")
+  .attr("x", spacing)
+  .attr("y", whiteHeight + offset)
+  .text("1937")
+  .style("fill", "black")
+  .style("font-size", "14");
+
+legend.append("text")
+  .attr("id", "legendText")
+  .attr("x", spacing)
+  .attr("y", redHeight+offset)
+  .text("2017")
+  .style("fill", "black")
+  .style("font-size", "14");
+
+
+//end handmade legend stuff
+
 
 });
